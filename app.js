f0daf93bea93bdc14bacae1c4127eb6417f6f1fa
Robbie Prokop <robbieprokop@gmail.com>
2022-10-24 19:16:06 +0400

    get app working

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// //Book COnstructor
-// function Book(title, author, isbn) {
-//   this.title = title;
-//   this.author = author;
-//   this.isbn = isbn;
-// }
+//Book COnstructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
 
-// //UI Constructor
-// function UI() {}
+//UI Constructor
+function UI() {}
 
 // Event Listeners
-// document.getElementById("book-form").addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   console.log("test");
-// });
-
-console.log("hello");
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("test");
+});
