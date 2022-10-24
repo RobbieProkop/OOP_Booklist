f8b732df54e8c3c90d35630667aa7eca0880ffe4
Robbie Prokop <robbieprokop@gmail.com>
2022-10-24 19:30:59 +0400

    add book to list, clear fields

--- app.js
+++ app.js
@@ -8,8 +8,46 @@ function Book(title, author, isbn) {
 //UI Constructor
 function UI() {}
 
+UI.prototype.addBookToList = (book) => {
+  const list = document.getElementById("book-list");
+
+  //create tr element
+  const row = document.createElement("tr");
+  //insert columns
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+  list.appendChild(row);
+};
+
+//clear fields
+UI.prototype.clearFields = () => {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
 // Event Listeners
-document.getElementById("book-form").addEventListener("submit", function (e) {
+document.getElementById("book-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("test");
+
+  //get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  //instantiate UI
+  const ui = new UI();
+
+  //add book to list
+  ui.addBookToList(book);
+
+  //clear fields
+  ui.clearFields();
 });
